489a1ec7fac8976fbca17810e465d27aac2cea2b
rasathelamedude <rasyar.safen19@gmail.com>
2026-03-21 22:59:44 +0300

    Initiate celery backgroun workers on start-up + Update Dockerfile to user gunicorn for production

--- CreditApproval/CreditApproval/settings.py
+++ CreditApproval/CreditApproval/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "credit_approval",
     "rest_framework",
+    "credit_approval.apps.CreditApprovalConfig",
 ]
 
 MIDDLEWARE = [
--- CreditApproval/credit_approval/apps.py
+++ CreditApproval/credit_approval/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class CreditApprovalConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'credit_approval'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "credit_approval"
+
+    def ready(self):
+        from .tasks import ingest_customer_data, ingest_loan_data
+
+        ingest_customer_data.delay()
+        ingest_loan_data.delay()
